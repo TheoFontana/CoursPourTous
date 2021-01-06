69135448cf27e8419392cacb7165e8b3d0f123af
TheoFontana <62117532+TheoFontana@users.noreply.github.com>
2021-01-06 17:30:39 +0100

    project update

--- js/prof.js
+++ js/prof.js
@@ -27,7 +27,7 @@ function makeSmall(event) {
 // Main
 var profs = [];
 var closes=[];
-for (var i =0 ;i<3;i++) {
+for (var i =0 ;i<6;i++) {
     profs[i]="prof-"+(i+1);
     closes[i]="close-"+(i+1);
 }
@@ -36,3 +36,45 @@ for(i=0;i<profs.length;i++){
     document.getElementById(profs[i]).addEventListener("click", makeBig);
     document.getElementById(closes[i]).addEventListener("click", makeSmall);
 }
+
+
+// Create a condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia('(max-width: 900px)');
+const media = window.matchMedia('(min-width: 901px)');
+
+function handleDesktopChange(e){
+
+    if (e.matches){
+        for(var i=0;i<profs.length;i++){
+        
+            prof=document.getElementById(profs[i]);
+            prof.addEventListener("click", makeBig)
+            prof.classList.remove("active");
+        }
+        console.log('Desktop');
+    }
+
+}
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+
+    for(var i=0;i<profs.length;i++){
+        
+        prof=document.getElementById(profs[i]);
+        prof.removeEventListener("click", makeBig)
+        prof.classList.add("active");
+    }
+    // Then log the following message to the console
+    console.log('Tablet');
+  }
+}
+
+// Register event listener
+mediaQuery.addListener(handleTabletChange);
+media.addListener(handleDesktopChange);
+
+// Initial check
+handleTabletChange(mediaQuery);
+handleDesktopChange(media);
